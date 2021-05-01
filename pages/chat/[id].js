37946731639b5ff1263451083a544df9a9c591a5
Atharva <atharva1525@gmail.com>
2021-05-01 23:44:48 +0530

    chat id page added

--- pages/chat/[id].js
+++ pages/chat/[id].js
@@ -1,18 +1,77 @@
 import styled from "styled-components";
+import Head from "next/head";
+import Sidebar from "../../components/Sidebar";
+import ChatScreen from "../../components/ChatScreen";
+import { db, auth } from "../../firebase";
+import getRecipientEmail from "../../utils/getRecipientEmail";
+import { useAuthState } from "react-firebase-hooks/auth";
 
-function Chat() {
+function Chat({ chat, messages }) {
+  const [user] = useAuthState(auth);
+
   return (
     <Container>
       <Head>
-        <title>Chat</title>
+        <title>Chat with {getRecipientEmail(chat.users, user)}</title>
       </Head>
-      <h1>this is ad</h1>
+      <Sidebar />
+      <ChatContainer>
+        <ChatScreen chat={chat} messages={messages} />
+      </ChatContainer>
     </Container>
   );
 }
 
 export default Chat;
 
-const Container = styled.div``;
+export async function getServerSideProps(context) {
+  const ref = db.collection("chats").doc(context.query.id);
 
-const Head = styled.div``;
+  // PREP the messages on the server
+  const messagesRes = await ref
+    .collection("messages")
+    .orderBy("timestamp", "asc")
+    .get();
+
+  const messages = messagesRes.docs
+    .map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }))
+    .map((messages) => ({
+      ...messages,
+      timestamp: messages.timestamp.toDate().getTime(),
+    }));
+
+  //PREP the chats
+  const chatRes = await ref.get();
+  const chat = {
+    id: chatRes.id,
+    ...chatRes.data(),
+  };
+
+  console.log(chat, messages);
+
+  return {
+    props: {
+      messages: JSON.stringify(messages),
+      chat: chat,
+    },
+  };
+}
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const ChatContainer = styled.div`
+  flex: 1;
+  overflow: screen;
+  height: 100vh;
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
+  -ms-overflow-style: none; // IE and Edge
+  scrollbar-width: none; // Firefox
+`;
